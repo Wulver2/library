a6eabd484253180019b48f9cdcb679641342b11c
Wulver2 <ross.shayla21@gmail.com>
2025-08-28 10:36:47 -0400

    Created array to hold all books in library, a constructor for those books(not set up), and a function to add those books to the array(not set up)

---
+++ library.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book() {
+    this.ID = crypto.randomUUID();
+};
+
+function addBookToLibrary() {
+    //creates book object from paramaters and stores it in myLibrary
+};
