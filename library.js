e561e96695ca261eb2a6a13de39d7730a90c062d
Wulver2 <ross.shayla21@gmail.com>
2025-08-28 10:46:33 -0400

    linked html file to library.js and completed the book constructor and addBookToLibrary function

--- library.js
+++ library.js
@@ -1,9 +1,17 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
     this.ID = crypto.randomUUID();
 };
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages) {
     //creates book object from paramaters and stores it in myLibrary
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
 };
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
+console.log(myLibrary.pop().author);
