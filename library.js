9c91cdd5b859b56422344931bedd96cca580ee53
Wulver2 <ross.shayla21@gmail.com>
2025-09-02 12:28:55 -0400

    added function to book object that when called returns a string of the book's title, author, and # of pages. Additionally created two functions to help print out everything in myLibrary

--- library.js
+++ library.js
@@ -1,10 +1,15 @@
 const myLibrary = [];
 
 function Book(title, author, pages) {
+    //Book constructor -- could add other properties
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.ID = crypto.randomUUID();
+
+    this.info = function() {
+        return `${this.title}` + " by " + `${this.author}` + ", " + `${this.pages}` + " pages"
+    }
 };
 
 function addBookToLibrary(title, author, pages) {
@@ -13,5 +18,20 @@ function addBookToLibrary(title, author, pages) {
     myLibrary.push(newBook);
 };
 
+function displayLibrary() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        makeCard(myLibrary[i]);
+    }
+};
+
+function makeCard(book) {
+    let container = document.querySelector(".container");
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.textContent = book.info();
+    container.appendChild(card);
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
-console.log(myLibrary.pop().author);
+
+displayLibrary();
